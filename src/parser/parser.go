cfaa40c8c91590fb3d339e8b10954d8d8d9d0cf6
Roman Dymchenko <r.dimchenko@x12.tech>
2018-05-26 05:20:07 +0600

    test different price state

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -3,14 +3,46 @@ package parser
 import (
 	"io"
 	"log"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func Parse(r io.Reader) string {
+type AmazonCoUkParsedData struct {
+	Title     string `json:"title"`
+	Price     string `json:"price"`
+	Image     string `json:"image"`
+	Available bool   `json:"available"`
+}
+type AmazonCoUkBulkData struct {
+	Url  string               `json:"url"`
+	Meta AmazonCoUkParsedData `json:"meta"`
+}
+
+func Parse(r io.Reader) AmazonCoUkParsedData {
 	doc, err := goquery.NewDocumentFromReader(r)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return doc.Find("span.a-color-price").First().Text()
+	result := AmazonCoUkParsedData{}
+	buyBox := doc.Find("div#buybox")
+
+	avail := buyBox.Find("div#availability")
+	if avail.Length() > 0 {
+		result.Available = strings.Contains(avail.Text(), "In stock")
+	}
+
+	price := buyBox.Find("span.a-color-price")
+	if price.Length() > 0 {
+		result.Price = strings.TrimFunc(price.Text(), func(c rune) bool {
+			if strings.ContainsAny("0123456789.", string(c)) {
+				return false
+			}
+			return true
+		})
+	}
+
+	//	q, _ := json.MarshalIndent(result, "", "\t")
+	//	fmt.Println(string(q))
+	return result
 }
